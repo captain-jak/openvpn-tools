3de5b383d95fe68441d76664ebb4f14716239260
captain-jak <bean@selfmicro.fr>
2021-03-25 08:05:42 +0100

    Change

--- distant.sh
+++ distant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Mise à jour du serveur:
+# Mise à jour du serveur openvpn:
 echo "Mise à jour openvpn.selfmicro.com"
 cd ~/openvpn-tools
 git pull
--- ok.sh
+++ ok.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# On pousse les nouveaux fichiers sur le serveur:
+# On pousse les nouveaux fichiers sur github
 git commit -a -m "Change"
 git push
 
-# Mise à jour du serveur:
+# Mise à jour sur le serveur openvpn:
 DISPLAY=1 SSH_ASKPASS="/home/enjoy/.ssh/x" ssh-add ~/.ssh/digital-ocean < /dev/null
 ssh -i ~/.ssh/digital-ocean root@openvpn.selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
 
