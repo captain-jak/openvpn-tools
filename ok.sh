c5a3779c515713e35f815613989002201202ada3
captain-jak <bean@selfmicro.fr>
2021-03-25 05:04:40 +0100

    Change

--- ok.sh
+++ ok.sh
@@ -5,4 +5,6 @@ git commit -a -m "Change"
 git push
 
 # Mise à jour du serveur:
+DISPLAY=1 SSH_ASKPASS="/home/enjoy/.ssh/x" ssh-add ~/.ssh/digital-ocean < /dev/null
 ssh -i ~/.ssh/digital-ocean root@openvpn.selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
+
