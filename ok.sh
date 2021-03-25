36a59525e4ae830e55c126fddc603c6d6523b242
captain-jak <bean@selfmicro.fr>
2021-03-25 04:50:28 +0100

    Change

--- ok.sh
+++ ok.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# On pousse les nouveaux fichiers sur le serveur:
 git commit -a -m "Change"
 git push
+
+# Mise à jour du serveur:
+ssh -i ~/.ssh/digital-ocean root@openvpn.selfmicro.com 'bash -s' < local_distant.sh
