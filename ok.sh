4b416adb4a89cbe783ce5045bd38ffa2799f2d7a
captain-jak <bean@selfmicro.fr>
2021-04-06 11:00:12 +0200

    Changement par script sur Lenovo3

--- ok.sh
+++ ok.sh
@@ -6,5 +6,5 @@ git commit -a -m "Changement par script sur Lenovo3"
 git push
 
 # Mise à jour sur le serveur alibaba:
-DISPLAY=1 SSH_ASKPASS="/home/enjoy/.ssh/x" ssh-add ~/.ssh/alibaba-openvpn < /dev/null
-ssh -i ~/.ssh/alibaba-openvpn root@openvpn.selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
+DISPLAY=1 SSH_ASKPASS="/home/enjoy/.ssh/x" ssh-add ~/.ssh/selfmicro-alibaba < /dev/null
+ssh -i ~/.ssh/selfmicro-alibaba root@openvpn.selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
