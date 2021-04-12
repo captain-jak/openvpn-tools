b9506719315d25e2e55ae943d3e56bf0c1c1c1b6
captain-jak <bean@selfmicro.fr>
2021-04-12 06:44:48 +0200

    Changement par script sur Lenovo3

--- ok.sh
+++ ok.sh
@@ -9,7 +9,7 @@ git push
 # Mise à jour sur le serveur alibaba:
 #DISPLAY=1 SSH_ASKPASS="/home/enjoy/.ssh/x" ssh-add ~/.ssh/selfmicro-alibaba.pem < /dev/null
 ssh -i ~/.ssh/selfmicro-alibaba.pem root@openvpn.selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
-echo "\e[1;31mMise à jour sur serveur Alibaba faite\e[0m"
+echo "Mise à jour sur serveur Alibaba faite"
 
 # Mise à jour sur le serveur contabo:
 ssh -i ~/.ssh/git-contabo root@selfmicro.com 'bash -s' < /home/enjoy/openvpn-tools/distant.sh
